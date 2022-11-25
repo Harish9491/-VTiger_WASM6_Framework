ab22f39f32127caaf6cb8f2b6ec2ac1ae79ccef0
haris <haris@HARISH-PC>
2022-11-25 22:32:56 +0530

    commit Test pipelining closing browser

--- src/test/java/vtiger/Practice/ReadDataFromCMDline.java
+++ src/test/java/vtiger/Practice/ReadDataFromCMDline.java
@@ -16,6 +16,7 @@ public class ReadDataFromCMDline {
 		System.out.println("Password given is --> "+PASSWORD);
 		System.out.println("Password given is --> "+PASSWORD);
 		System.out.println("Password given is --> "+PASSWORD);
+		System.out.println("Password given is --> "+PASSWORD);
 		
 	}
 
