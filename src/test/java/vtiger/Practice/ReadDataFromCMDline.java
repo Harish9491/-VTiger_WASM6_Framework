4e5e4ce3d6429efe12ec4dd37ab2a32227dcdbc0
haris <haris@HARISH-PC>
2022-11-25 19:44:10 +0530

    Changes in read suite with multiple password

--- src/test/java/vtiger/Practice/ReadDataFromCMDline.java
+++ src/test/java/vtiger/Practice/ReadDataFromCMDline.java
@@ -14,6 +14,8 @@ public class ReadDataFromCMDline {
 		System.out.println("Browser given is --> "+BROWSER);
 		System.out.println("Username given is --> "+USERNAME);
 		System.out.println("Password given is --> "+PASSWORD);
+		System.out.println("Password given is --> "+PASSWORD);
+		System.out.println("Password given is --> "+PASSWORD);
 	}
 
 }
